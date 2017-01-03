cea61b471460eafe7d443b7897a944e81d6a69b4
nicolagreco <me@nicolagreco.com>
2017-01-03 12:25:38 +0100

    adding new images

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 for file in 2017/*.jpg; do
-  convert -thumbnail 700x700^ -gravity center -extent 700x700 "$file" "$file"-thumb-retina.png
-  convert -thumbnail 350x350^ -gravity center -extent 350x350 "$file" "$file"-thumb.png
+#  convert -thumbnail 700x700 -gravity center -extent 700x700 "$file" "$file"-thumb-retina.png
+#  convert -thumbnail 350x350 -gravity center -extent 350x350 "$file" "$file"-thumb.png
+   convert -thumbnail 700x700 "$file" "$file"-thumb-retina.jpg
+   convert -thumbnail 1460x1460 "$file" "$file"-thumb-special.jpg
 done
