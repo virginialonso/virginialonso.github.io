3bd2ff9f3ee027ec6a0eecc37730f90323391e68
nicolagreco <me@nicolagreco.com>
2017-01-03 14:41:55 +0100

    adding latest updates

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-for file in 2017/*.jpg; do
+for file in new_2017/*.jpg; do
 #  convert -thumbnail 700x700 -gravity center -extent 700x700 "$file" "$file"-thumb-retina.png
 #  convert -thumbnail 350x350 -gravity center -extent 350x350 "$file" "$file"-thumb.png
    convert -thumbnail 700x700 "$file" "$file"-thumb-retina.jpg
    convert -thumbnail 1460x1460 "$file" "$file"-thumb-special.jpg
+   # composite -gravity southeast images/sign2.png "$file" new_"$file"
+   # composite -gravity southeast images/sign2c.png "$file" new_"$file"
 done
